52e7e8ebc5be7c3a21c59c5599eb282694e04b13
Lâm Trung Hiếu <trunghieu.lam.31@gmail.com>
2022-08-13 11:31:56 +0700

    Update quick_start.sh

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,9 +1,9 @@
 if [ ! -f assets/infore/hifigan/g_01140000 ]; then
   echo "Downloading models..."
   mkdir -p assets/infore/{nat,hifigan}
-  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/duration_latest_ckpt.pickle -O assets/infore/nat/duration_latest_ckpt.pickle
-  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/acoustic_latest_ckpt.pickle -O assets/infore/nat/acoustic_latest_ckpt.pickle
-  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/g_01140000 -O assets/infore/hifigan/g_01140000
+  wget https://huggingface.co/trunghieulam/viettts/resolve/main/duration_latest_ckpt.pickle -O assets/infore/nat/duration_latest_ckpt.pickle
+  wget https://huggingface.co/trunghieulam/viettts/resolve/main/acoustic_latest_ckpt.pickle -O assets/infore/nat/acoustic_latest_ckpt.pickle
+  wget https://huggingface.co/trunghieulam/viettts/resolve/main/g_01140000 -O assets/infore/hifigan/g_01140000
   python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_01140000
 fi
 
